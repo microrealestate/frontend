2d65432a34b568a24bef4676f19fa0de1e77609f
Camel Aissani <camel.aissani@nuageprive.fr>
2020-12-19 14:35:54 +0100

    fixed redirect function to take in account the baseurl

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 export const isServer = () => typeof window === 'undefined';
 
-export const redirect = (context, Location) => {
-    context.res.writeHead(302, { Location })
+export const redirect = (context, path) => {
+    context.res.writeHead(302, { Location: `/app${path}` })
     context.res.end()
 }
